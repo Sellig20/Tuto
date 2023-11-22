995f89e4873258434447b00c19b0a0e4847ffeb4
Jeanne Colmou <jeannecolmou@mbp-de-jeanne.home>
2023-11-22 19:04:07 +0100

    bug connexion bachend-frontend GET/POST/PUT 404

---
+++ part1/src/main.js
@@ -0,0 +1,37 @@
+
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+
+// import axios from 'axios'
+
+// const promise = axios.get('http://localhost:3001notes')
+
+// promise.then(response => {
+//     const notes = response.data
+//     console.log(notes)
+// })
+
+// console.log("boujoujou");
+// // const promise = axios.get('https://localhost:3001/notes')
+// // console.log(promise)
+
+// axios.get('http://localhost:3001/notes').then(response => {
+//     const notes = response.data
+//     console.log(notes)
+// })
+
+//
+/*
+axios
+    .get('http....)
+    .then(response => {
+        const notes = response.data
+        console.log(notes)
+    })
+*/
+
+// const promise2 = axios.get('https://localhost:3001/foobar')
+// console.log(promise2)
